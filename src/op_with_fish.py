063e250c84ec4643b295e1c7a72243a56c26febc
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-11-24 19:29:15 +0900

    Add fisher info

--- src/op_with_fish.py
+++ src/op_with_fish.py
@@ -8,9 +8,10 @@ length = len(script_dir)
 script_dir.pop(length - 1)
 script_dir = "/".join(script_dir)
 
+
 def operation_with_fish(all: bool = False):
-    configure_bobthefish()
-    install_powerline()
+    # configure_bobthefish()
+    # install_powerline()
 
     home = os.getenv("HOME")
     sh.run_as_fish(f"source {home}/.config/fish/config.fish")
@@ -19,12 +20,33 @@ def operation_with_fish(all: bool = False):
     sh.direct(f"{script_dir}/logo.sh")
     print_b("All settings are completed!!!")
 
+    print("------------------info------------------")
+    intro_install_fisher()
+    intro_nvm()
+    print("----------------------------------------")
+
+
+def intro_install_fisher():
+    print_b("To install fisher:")
+    print(
+        f"curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher")
+    print()
+
+
+def intro_nvm():
+    print_b("To install nvm")
+    print("fisher install jorgebucaran/nvm.fish")
+    print()
+
+
 def configure_bobthefish():
     print_b("Configuring theme...")
     if not omf_exsists():
-        sh.run("curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish")
+        sh.run(
+            "curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish")
     sh.run_as_fish("omf update")
     sh.run_as_fish("omf install bobthefish")
+
 
 def install_powerline():
     print_b("Installing fonts...")
@@ -34,10 +56,10 @@ def install_powerline():
     os.chdir("..")
     sh.direct("rm -rf fonts")
 
+
 def omf_exsists() -> bool:
     try:
         sh.run_as_fish("omf > /dev/null")
         return True
     except:
         return False
-
